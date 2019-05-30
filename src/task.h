c165fdba1e5c1e938b6bc0bcc46c247051a5621b
vic-lee <viclee@nyu.edu>
2019-05-30 18:36:56 -0400

    Update task constructor to include IDs

--- src/task.h
+++ src/task.h
@@ -10,6 +10,6 @@ struct task_t
     tid_t tid;
     tid_t ptid;
     std::string name;
-    task_t(std::string name) : name(name){};
+    task_t(std::string name, tid_t tid, tid_t ptid) : tid(tid), ptid(ptid), name(name){};
 };
 } // namespace tasks
