60bd4eecb5e4ebabab8e71d0f0622640d7104feb
vic-lee <viclee@nyu.edu>
2019-05-30 16:57:38 -0400

    Add function to detect indent

--- src/planreader.cc
+++ src/planreader.cc
@@ -1,13 +1,32 @@
 #include "fstream"
 #include "iostream"
 
+size_t check_indent(const std::string &line, const size_t linectr)
+{
+    size_t indent = 0;
+    for (const char &c : line)
+    {
+        if (c > ' ')
+            break;
+        else
+            indent++;
+    }
+    printf("~%zu \tindent (%zu):", linectr, indent);
+    for (int i = 0; i < indent; i++)
+        printf("_");
+    printf("\n");
+    return indent;
+}
+
 int main(int argc, char *argv[])
 {
     std::ifstream plan("in/sample.md");
     std::string line;
+    size_t linectr = 0;
     while (getline(plan, line))
     {
-        std::cout << line << std::endl;
+        check_indent(line, linectr);
+        linectr++;
     }
     return 0;
 }
