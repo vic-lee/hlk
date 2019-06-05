01d3312f72b11b7b36c3e893af9fa849a683f7a4
vic-lee <viclee@nyu.edu>
2019-06-05 15:57:33 -0400

    Rename vars

--- src/planreader.cc
+++ src/planreader.cc
@@ -43,16 +43,16 @@ void read_tasks(tasks::TaskManager& tmg, const std::string& fpath)
         size_t linectr     = 0;
         size_t prev_indent = 0;
         tasks::tid_t prev_tid;
-        tasks::tid_t cur_ptid = tasks::TaskManager::ROOT_;
+        tasks::tid_t curr_ptid = tasks::TaskManager::ROOT_;
         while (getline(plan, line))
         {
-            unsigned int ic   = 0;
-            size_t cur_indent = io::check_indent(line, linectr, ic);
+            unsigned int ic    = 0;
+            size_t curr_indent = io::check_indent(line, linectr, ic);
 
-            if (cur_indent > prev_indent) cur_ptid = prev_tid;
+            if (curr_indent > prev_indent) curr_ptid = prev_tid;
 
-            prev_tid    = tmg.add_task(line.substr(ic + 2), cur_ptid);
-            prev_indent = cur_indent;
+            prev_tid    = tmg.add_task(line.substr(ic + 2), curr_ptid);
+            prev_indent = curr_indent;
             linectr++;
         }
     }
